5eeb1fe8f02e957677bea92394e6261bd5611f0d
Kadiro Elemo <you@example.com>
2018-02-18 02:22:16 -0600

    Get Twits.

---
+++ twitget.js
@@ -0,0 +1,19 @@
+var Twit = require ('twit');
+
+var config = require ('./config.js');
+
+var T = new Twit(config);
+
+var params = {
+	q: "opride",
+	count:10
+}
+
+T.get("search/tweets", params, gotData);
+
+function gotData(err, data, response){
+	var tweets = data.statuses;
+	for(var i =0; i < tweets.length; i++) {
+		console.log(tweets[i].text);
+	}
+}
